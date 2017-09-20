611d650344d6246d298549adf0b7f8028fca9ba1
cacody <cacody@umich.edu>
2017-09-20 16:58:29 -0400

    last commit before branch to fix

--- dothings.php
+++ dothings.php
@@ -8,12 +8,12 @@ $firstname = $_POST['first_name'];
 $lastname = $_POST['last_name'];
 echo $firstname . " " .$lastname;
 echo '<br>';
-$myPath getcwd()."/pdfs/";
+$myPath getcwd().'/pdfs/';
 echo '<hr><br>';
 //phpinfo();
 
 
-require_once "./classes/tcpdf/tcpdf.php";
+require_once "classes/tcpdf/tcpdf.php";
 $obj_pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 $obj_pdf->SetCreator(PDF_CREATOR);
 $obj_pdf->SetTitle("Export HTML table data using TCPDF");
@@ -35,8 +35,9 @@ $pdffilename = 'RECORD_'.date('YmdHis').'.pdf';
 $obj_pdf->writeHTML($content);
 $obj_pdf->Output($pdffilepath.$pdffilename, 'F');
 
+echo $pdffilepath.$pdffilename;
 
-require './classes/PHPMailer_5.2.1/class.phpmailer.php';
+require 'classes/PHPMailer_5.2.1/class.phpmailer.php';
 $mail = new PHPMailer;
 $mail->setFrom('barleyandhops@beerworld.com', "$firstname");
 $mail->addAddress('cacody@umich.edu', 'My Friend');
