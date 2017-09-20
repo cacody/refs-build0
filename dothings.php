9adefec3db7289a3184ba2777aa17ba1272b80d7
cacody <cacody@umich.edu>
2017-09-20 14:53:00 -0400

    adding ./ to upload paths

--- dothings.php
+++ dothings.php
@@ -13,7 +13,7 @@ echo '<hr><br>';
 phpinfo();
 
 
-require_once "classes/tcpdf/tcpdf.php";
+require_once "./classes/tcpdf/tcpdf.php";
 $obj_pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 $obj_pdf->SetCreator(PDF_CREATOR);
 $obj_pdf->SetTitle("Export HTML table data using TCPDF");
@@ -36,7 +36,7 @@ $obj_pdf->writeHTML($content);
 $obj_pdf->Output($pdffilepath.$pdffilename, 'F');
 
 
-require 'classes/PHPMailer_5.2.1/class.phpmailer.php';
+require './classes/PHPMailer_5.2.1/class.phpmailer.php';
 $mail = new PHPMailer;
 $mail->setFrom('barleyandhops@beerworld.com', "$firstname");
 $mail->addAddress('cacody@umich.edu', 'My Friend');
