2641fed6d1cf9a4b05b3e1837c70e7e5e9a47e9b
cacody <cacody@umich.edu>
2017-09-21 10:32:01 -0400

    got local computed path fixed for localdev

--- dothings.php
+++ dothings.php
@@ -8,7 +8,8 @@ $firstname = $_POST['first_name'];
 $lastname = $_POST['last_name'];
 echo $firstname . " " .$lastname;
 echo '<br>';
-$myPath getcwd().'/pdfs/';
+$pdffilepath = getcwd() . "/pdfs" . "/";
+echo $pdffilepath;
 echo '<hr><br>';
 //phpinfo();
 
@@ -17,10 +18,6 @@ require_once "classes/tcpdf/tcpdf.php";
 $obj_pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 $obj_pdf->SetCreator(PDF_CREATOR);
 $obj_pdf->SetTitle("Export HTML table data using TCPDF");
-$obj_pdf->SetHeaderData('', '', PDF_HEADER_TITLE, PDF_HEADER_STRING);
-$obj_pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
-$obj_pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
-$obj_pdf->SetDefaultMonospacedFont('helvetica');
 $obj_pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
 $obj_pdf->SetMargins(PDF_MARGIN_LEFT, '5', PDF_MARGIN_RIGHT);
 $obj_pdf->setPrintHeader(false);
@@ -29,13 +26,10 @@ $obj_pdf->SetAutoPageBreak(TRUE, 10);
 $obj_pdf->SetFont('helvetica', '', 12);
 $obj_pdf->addPage();
 $content = '';
-$content .= "<h3 align=\"center\">$firstname $lastname</h3>";
-$pdffilepath = $myPath;
-$pdffilename = 'RECORD_'.date('YmdHis').'.pdf';
+$content .= "<p>$firstname $lastname</p>";
+$pdffilename = $firstname . '-' . $lastname.'.pdf';
 $obj_pdf->writeHTML($content);
-$obj_pdf->Output("$pdffilepath.$pdffilename", 'F');
-
-echo $pdffilepath.$pdffilename;
+$obj_pdf->Output($pdffilepath.$pdffilename, 'F');
 
 require 'classes/PHPMailer_5.2.1/class.phpmailer.php';
 $mail = new PHPMailer;
